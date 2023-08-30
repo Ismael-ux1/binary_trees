d76f5096574848353418d8a3eed28406745303d8
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-08-30 12:08:23 -0700

    updated the hight function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,20 +6,18 @@
  * Return: The height of the tree, or 0 if tree is NULL
  *
  * Description: This function calculates the height of a binary tree,
- * which is the lengt of the longest path from the root node to a leaf.
+ * which is the length of the longest path from the root node to a leaf.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
-		return (0);
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 
-	/* Recursively calculate the height of the left and right subtrees */
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
-
-	/* Return the maximum of the left and right subtree heights */
-	 /* plus 1 for the current node */
-	return (height_left > height_right ? height_left + 1 : height_right + 1);
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
