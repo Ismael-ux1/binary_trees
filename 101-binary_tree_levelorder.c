84ac8864c8e1631be0b5a770565952a8a77b6e74
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-09-01 18:11:43 -0700

    Level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -33,7 +33,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 		func(node->n);
 
 		/* Enqueue the left child if it exists */
-		if (node->leftt != NULL)
+		if (node->left != NULL)
 			queue[back++] = node->left;
 
 		/* Enqueue the right child if it exists */
