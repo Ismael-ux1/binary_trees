9975d2edac431f851850749d145b4a04c3ad1148
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-09-03 15:16:18 -0700

    Fixed BST-insert function

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,63 +1,44 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * bst_insert - inserts a value in a BST
- * @tree: double pointer to the root node
- * @value: value to store in the node
+ * bst_insert - Inserts a value into a Binary Search Tree (BST).
+ * @tree: Double pointer to the root node of the BST to insert the value.
+ * @value: Value to store in the node to be inserted.
  *
- * Return: pointer to the created node, or NULL
+ * Return: Pointer to the created node, or NULL on failure.
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *new_node = NULL;
-	bst_t *current = NULL;
-
-	if (tree == NULL)
+	if (!tree)
 		return (NULL);
 
-	new_node = malloc(sizeof(bst_t));
-	if (new_node == NULL)
+	if (!*tree)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+
+	if (value == (*tree)->n)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = NULL;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	if (*tree == NULL)
+	if (value < (*tree)->n)
 	{
-		*tree = new_node;
-		return (new_node);
+		if (!(*tree)->left)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		return (bst_insert(&((*tree)->left), value));
 	}
-
-	current = *tree;
-	while (current != NULL)
+	else
 	{
-		if (value < current->n)
+		if (!(*tree)->right)
 		{
-			if (current->left == NULL)
-			{
-				current->left = new_node;
-				new_node->parent = current;
-				return (new_node);
-			}
-			current = current->left;
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
 		}
-		else if (value > current->n)
-		{
-			if (current->right == NULL)
-			{
-				current->right = new_node;
-				new_node->parent = current;
-				return (new_node);
-			}
-			current = current->right;
-		}
-		else
-		{
-			free(new_node);
-			return (NULL);
-		}
+		return (bst_insert(&((*tree)->right), value));
 	}
-	return (NULL);
 }
+
